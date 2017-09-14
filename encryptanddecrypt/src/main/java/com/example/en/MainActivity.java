7a769f04abe2b8112c088edbcab037e2e4b70468
luhouhua <luhh007@163.com>
2017-09-14 17:53:32 +0800

    加密解密

--- encryptanddecrypt/src/main/java/com/example/en/MainActivity.java
+++ encryptanddecrypt/src/main/java/com/example/en/MainActivity.java
@@ -13,10 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String publicKey = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDzLtcxM9+f/qH0K1e38u2gTLu+sB2AOgUmrj3pUO2Rmlb/ch9wRUU08KfHozArBIL1Dj5xgyC89mWbADjmbI/um9m8z7AGy9kGaL6J0uQ2oFmIqeiqlLr5a4s304Cb24SEikeLtWWBeE01kfIOsi32Y8w5Yuc7yv5JZSFoO9kwvQIDAQAB";
-
-        String str = "8n4q8RwlJKrmxuktAPkAGMJDDigYlhmHIAYgxa/JHlll5RM7L90IfqgxDmQOExrmH8/kFp5Ot+ivaB5d9UKOtU5WAVsATJjJ7o0+U40kc5fLX6PRVYIB52agJXBzDRYdJnzQxovex/1aqQi6ZrLSpKTw1Ml0CIGXeJ+/bhCgesA=";
-
         try {
            RSAUtils.getKeys();
         } catch (Exception e) {
